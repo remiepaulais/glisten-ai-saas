3dab017ef3315b94ac260b2359b3ac11f7c8c71c
Rémi Epaulais <epaulais.remi@gmail.com>
2024-12-03 17:24:21 +0100

    Adde Hero Slice

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,21 +1,21 @@
 import {
   SliceSimulator,
   SliceSimulatorParams,
-  getSlices,
-} from "@slicemachine/adapter-next/simulator";
-import { SliceZone } from "@prismicio/react";
+  getSlices
+} from '@slicemachine/adapter-next/simulator'
+import { SliceZone } from '@prismicio/react'
 
-import { components } from "@/slices";
+import { components } from '@/slices'
 
 export default async function SliceSimulatorPage({
-  searchParams,
+  searchParams
 }: SliceSimulatorParams) {
-  const { state } = await searchParams;
-  const slices = getSlices(state);
+  const { state } = await searchParams
+  const slices = getSlices(state)
 
   return (
-    <SliceSimulator>
+    <SliceSimulator background='#070815'>
       <SliceZone slices={slices} components={components} />
     </SliceSimulator>
-  );
+  )
 }
